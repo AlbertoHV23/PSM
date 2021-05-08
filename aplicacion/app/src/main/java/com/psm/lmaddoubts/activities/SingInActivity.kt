a59f56f708dfe5a83f4732f4cc698da2ca903133
AlbertoHV23 <42360698+AlbertoHV23@users.noreply.github.com>
2021-05-08 18:47:57 -0500

    cambios

--- aplicacion/app/src/main/java/com/psm/lmaddoubts/activities/SingInActivity.kt
+++ aplicacion/app/src/main/java/com/psm/lmaddoubts/activities/SingInActivity.kt
@@ -3,6 +3,7 @@ package com.psm.lmaddoubts.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.psm.lmaddoubts.R
 
 
 class SingInActivity : AppCompatActivity() {
