794b0e6a1c6f25b9d6abbeb2486b235745ad3d5d
Luis Gerardo Becerra Jiménez <gerajmz@outlook.com>
2021-05-16 01:40:00 -0500

    login webapi

--- doubts/App/Usuario.php
+++ doubts/App/Usuario.php
@@ -4,110 +4,141 @@ namespace App;
 
 use PDOException;
 
-class Usuario {
+class Usuario
+{
 
-	private $db;
-	function __construct($db){
-		$this->db = $db;
-	}
+    private $db;
+    function __construct($db)
+    {
+        $this->db = $db;
+    }
 
-	function Usuarios($id){
-		$orm = new \App\Core\Model($this->db);
+    function Usuarios($id)
+    {
+        $orm = new \App\Core\Model($this->db);
 
-       $filtro = array();
-	   if($id > 0){
-		  $filtro = array("id_usuario"=>$id);
-	   }
+        $filtro = array();
+        if ($id > 0) {
+            $filtro = array("id_usuario" => $id);
+        }
 
-		$list = $orm->select($filtro,
-							"tbl_usuario",
-							"mUsuario",
-							array(	"id_usuario"=>"id_usuario",
-                                    "nombre"=>"nombre",
-                                    "apellidos"=>"apellidos",
-									"email"=>"email",
-									"contrasena"=>"contrasena",
-									"avatar"=>"avatar"
-							),
-							"","","");
-		$items = iterator_to_array ($list);
+        $list = $orm->select($filtro,
+            "tbl_usuario",
+            "mUsuario",
+            array("id_usuario" => "id_usuario",
+                "nombre" => "nombre",
+                "apellidos" => "apellidos",
+                "email" => "email",
+                "contrasena" => "contrasena",
+                "avatar" => "avatar",
+            ),
+            "", "", "");
+        $items = iterator_to_array($list);
 
-		foreach($items as $item){
-			
-			$item->avatar = "data:image/png;base64," . base64_encode($item->imgArray);
-		
-		}
-		return  $items;
-	}
+        foreach ($items as $item) {
 
-	function Save($data){
+            $item->avatar = "data:image/png;base64," . base64_encode($item->avatar);
 
-		$orm = new \App\Core\Model($this->db);
-        
-		if($data["id_usuario"]>0){
+        }
+        return $items;
+    }
 
-			$instances = $this->Usuarios( $data["id_usuario"] );
+    function Save($data)
+    {
+
+        $orm = new \App\Core\Model($this->db);
+
+        if ($data["id_usuario"] > 0) {
+
+            $instances = $this->Usuarios($data["id_usuario"]);
             $instance = $instances[0];
-	
+
             $instance->nombre = $data["nombre"];
             $instance->apellidos = $data["apellidos"];
-			$instance->email =  $data["email"];
-			$instance->contrasena =  $data["contrasena"];
+            $instance->email = $data["email"];
+            $instance->contrasena = $data["contrasena"];
 
             $instance->avatar = $data["avatar"];
-            
-			if(isset($instance->imagen) ){
-				$datab = $instance->avatar;
-				list($type, $datab) = explode(';', $datab);
-				list(, $datab)      = explode(',', $datab);
-				$instance->avatar = base64_decode($datab);
-			}
 
-			return $orm->save($instance 
-							,"tbl_usuario"
-							,"id_usuario"
-							, array("id_usuario"=>"id_usuario",
-									"nombre"=>"nombre",
-									"apellidos"=>"apellidos",
-									"email"=>"email",
-									"contrasena"=>"contrasena",
-									"avatar"=>"avatar"));
-		
-		}else{
+            if (isset($instance->imagen)) {
+                $datab = $instance->avatar;
+                list($type, $datab) = explode(';', $datab);
+                list(, $datab) = explode(',', $datab);
+                $instance->avatar = base64_decode($datab);
+            }
 
+            return $orm->save($instance
+                , "tbl_usuario"
+                , "id_usuario"
+                , array("id_usuario" => "id_usuario",
+                    "nombre" => "nombre",
+                    "apellidos" => "apellidos",
+                    "email" => "email",
+                    "contrasena" => "contrasena",
+                    "avatar" => "avatar"));
 
-			if(isset($data["avatar"]) ){
-			$datab = $data["avatar"];
-				list($type, $datab) = explode(';', $datab);
-				list(, $datab)      = explode(',', $datab);
-				$data["avatar"] = base64_decode($datab);
-			}
+        } else {
 
-			return $orm->save($data 
-			,"tbl_usuario"
-			,"id_usuario"
-			, array("id_usuario"=>"id_usuario",
-					"nombre"=>"nombre",
-					"apellidos"=>"apellidos",
-					"email"=>"email",
-					"contrasena"=>"contrasena",
-					"avatar"=>"avatar"));
-		}
-	}
+            if (isset($data["avatar"])) {
+                $datab = $data["avatar"];
+                list($type, $datab) = explode(';', $datab);
+                list(, $datab) = explode(',', $datab);
+                $data["avatar"] = base64_decode($datab);
+            }
 
-	function Delete($data){
+            return $orm->save($data
+                , "tbl_usuario"
+                , "id_usuario"
+                , array("id_usuario" => "id_usuario",
+                    "nombre" => "nombre",
+                    "apellidos" => "apellidos",
+                    "email" => "email",
+                    "contrasena" => "contrasena",
+                    "avatar" => "avatar"));
+        }
+    }
+
+    function Delete($data)
+    {
 
         try {
-            $sentencia =  $this->db->prepare("DELETE FROM tbl_usuario WHERE id_usuario = ? " );
-		    $sentencia->bindParam(1,$data["id_usuario"]);
-		    $sentencia->execute();
+            $sentencia = $this->db->prepare("DELETE FROM tbl_usuario WHERE id_usuario = ? ");
+            $sentencia->bindParam(1, $data["id_usuario"]);
+            $sentencia->execute();
             return $mensaje = "Eliminado correctamente";
         } catch (PDOException $exc) {
             return $mensaje = "No se pudo eliminar";
         }
 
-		return array("mensaje"=>$mensaje);
-	}
+        return array("mensaje" => $mensaje);
+    }
 
+    function Exist($data)
+    {
+        $orm = new \App\Core\Model($this->db);
+
+        $filtro = array();
+
+        $filtro = array("email" => $data["email"], "contrasena" => $data["contrasena"]);
+
+        $list = $orm->select($filtro,
+            "tbl_usuario",
+            "mUsuario",
+            array("id_usuario" => "id_usuario",
+                "nombre" => "nombre",
+                "apellidos" => "apellidos",
+                "email" => "email",
+                "contrasena" => "contrasena",
+                "avatar" => "avatar",
+            ),
+            "", "", "");
+        $items = iterator_to_array($list);
+
+        foreach ($items as $item) {
+
+            $item->avatar = "data:image/png;base64," . base64_encode($item->avatar);
+
+        }
+        return $items;
+    }
 }
-?>
